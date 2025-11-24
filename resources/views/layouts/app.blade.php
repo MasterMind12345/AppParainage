a6e2a72dc9dac07939be013611e9532e3b102f00
danieldjakam <danidjakam@gmail.com>
2025-11-24 09:36:02 +0100

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('logo.jpeg') }}" type="image/x-icon">
 
 
     <!-- Scripts -->
