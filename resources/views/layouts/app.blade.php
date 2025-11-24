e858ad0f24009919f3ab9640e4a9b2bc8fe20507
danieldjakam <danidjakam@gmail.com>
2025-11-24 09:32:42 +0100

    build2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-    <link rel="icon" href="{{ Vite::asset('resources/favicon.ico') }}">
+    <link rel="icon" href="{{ Vite::asset('resources/images/favicon.ico') }}">
 
 
     <!-- Scripts -->
